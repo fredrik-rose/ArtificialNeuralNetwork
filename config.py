3b74383b9d1dd866120026abce8be56f70f80853
Fredrik Rose <fredrik.rose.developer@gmail.com>
2018-11-25 17:17:16 +0100

    Save and load digit classifer
    Save the digit classifer after training. Load a saved classifer if exists
    to avoid the need of training each time the script is run.

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ MNIST_TRAIN_IMAGES_PATH = os.path.join(_DIRNAME, 'Data/MNIST/train-images-idx3-u
 MNIST_TRAIN_LABELS_PATH = os.path.join(_DIRNAME, 'Data/MNIST/train-labels-idx1-ubyte/train-labels.idx1-ubyte')
 MNIST_TEST_IMAGES_PATH = os.path.join(_DIRNAME, 'Data/MNIST/t10k-images-idx3-ubyte/t10k-images.idx3-ubyte')
 MNIST_TEST_LABELS_PATH = os.path.join(_DIRNAME, 'Data/MNIST/t10k-labels-idx1-ubyte/t10k-labels.idx1-ubyte')
+SAVED_DIGIT_CLASSIFIER_PATH = os.path.join(_DIRNAME, 'Data/digit-classifer.pkl')
--- main.py
+++ main.py
@@ -2,11 +2,32 @@
 Program entry point.
 """
 import argparse
+import pickle
 
 import artificialneuralnetwork.application.digitclassifier as dc
 import artificialneuralnetwork.input.mnist as mnist
 
 import config
+
+
+def _save_object(obj, file_path):
+    """
+    Saves an object using pickle.
+    :param obj: Object to save.
+    :param file_path: Path to storage.
+    """
+    with open(file_path, 'wb') as file:  # NOTE: overwrites any existing file.
+        pickle.dump(obj, file, pickle.HIGHEST_PROTOCOL)
+
+
+def _load_object(file_path):
+    """
+    Loads an object using pickle.
+    :param file_path: Path to load object from.
+    :return: Loaded object.
+    """
+    with open(file_path, 'rb') as file:
+        return pickle.load(file)
 
 
 def _train(digit_classifier):
@@ -39,9 +60,13 @@ def main():
     parser.add_argument('-t', help='train the classifier', dest='train', action='store_true')
     parser.add_argument('-e', help='evaluate the classifier', dest='evaluate', action='store_true')
     args = parser.parse_args()
-    digit_classifier = dc.DigitClassifier(mnist.IMAGE_RESOLUTION)
+    try:
+        digit_classifier = _load_object(config.SAVED_DIGIT_CLASSIFIER_PATH)
+    except FileNotFoundError:
+        digit_classifier = dc.DigitClassifier(mnist.IMAGE_RESOLUTION)
     if args.train:
         _train(digit_classifier)
+        _save_object(digit_classifier, config.SAVED_DIGIT_CLASSIFIER_PATH)
     if args.evaluate:
         true_positive_rate = _evaluate(digit_classifier)
         print("TP: {0:.2f}%".format(true_positive_rate))
