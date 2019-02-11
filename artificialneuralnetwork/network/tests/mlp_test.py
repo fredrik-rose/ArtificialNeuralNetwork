2997dc5991c037b634fb6c39099ecd5d454b347c
Fredrik Rose <fredrik.rose.developer@gmail.com>
2019-02-11 18:16:39 +0100

    Use the analytical gradient verifier from utilities in the MLP tests

--- artificialneuralnetwork/network/tests/mlp_test.py
+++ artificialneuralnetwork/network/tests/mlp_test.py
@@ -61,43 +61,29 @@ def _verify_gradients(seed, layer_sizes, number_of_training_samples, regularizat
     :param dropout: Dropout probability.
     :param granularity: Granularity then comparing the analytical gradient with the numerical gradient.
     """
+    def cost(network, data):
+        """
+        Creates a cost function from a neural network.
+        :param network: Neural network.
+        :param data: Network data.
+        :return: Cost function.
+        """
+        def func():
+            """
+            Neural network cost function.
+            :return: Cost.
+            """
+            np.random.seed(seed)
+            return network._cost(data, regularization, dropout)
+
+        return func
+
     regularization /= number_of_training_samples
     data = util.generate_training_data((layer_sizes[0], 1), (layer_sizes[-1], 1), number_of_training_samples, seed)
     np.random.seed(seed)
     network = mlp.NeuralNetwork(layer_sizes)
+    cost_func = cost(network, data)
     np.random.seed(seed)
     bias_gradients, weight_gradients = network._gradient(data, regularization, dropout)
-    _verify_gradient_set(seed, data, network, network._biases, bias_gradients, regularization, dropout, granularity)
-    _verify_gradient_set(seed, data, network, network._weights, weight_gradients, regularization, dropout, granularity)
-
-
-def _verify_gradient_set(seed, data, network, layers, gradients, regularization, dropout, granularity, delta=1e-5):
-    """
-    Verifies a set, e.g. biases or weights, of gradients.
-    :param seed: Random seed used when calculating the analytical gradient.
-    :param data: Input to the network.
-    :param network: Neural network.
-    :param layers: Reference to the parameters of the network, e.g. biases or weights.
-    :param gradients: The parameters' corresponding analytical gradients.
-    :param regularization: Amount of regularization used when calculating the analytical gradient.
-    :param dropout: Dropout probability used when calculating the analytical gradient.
-    :param granularity: Granularity then comparing the analytical gradient with the numerical gradient.
-    :param delta: Delta to be used when calculating the numerical gradient.
-    """
-    assert len(gradients) == len(layers)
-    for layer, gradient in zip(layers, gradients):
-        assert gradient.shape == layer.shape
-        original_layer = np.copy(layer)
-        for index, analytical_gradient in np.ndenumerate(gradient):
-            layer[index] = original_layer[index] - delta
-            np.random.seed(seed)
-            left = network._cost(data, regularization, dropout)
-            layer[index] = original_layer[index] + delta
-            np.random.seed(seed)
-            right = network._cost(data, regularization, dropout)
-            layer[index] = original_layer[index]
-            numerical_gradient = (right - left) / (2 * delta)
-            if not analytical_gradient == numerical_gradient == 0:
-                relative_error = abs(analytical_gradient - numerical_gradient) / max(abs(analytical_gradient),
-                                                                                     abs(numerical_gradient))
-                assert relative_error <= granularity
+    util.verify_function_gradients(cost_func, network._biases, bias_gradients, granularity)
+    util.verify_function_gradients(cost_func, network._weights, weight_gradients, granularity)
